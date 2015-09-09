2ec8f83b0489e23a01cae2406f3653b4ecee0a78
algoldma <aidan.goldman@uvm.edu>
2015-09-09 13:54:41 -0400

    cs 148 set up
    assignment 1 complete with sitemap

---
+++ bin/pass.php
@@ -0,0 +1,6 @@
+<?php
+// passwords for your database 
+$dbReader="SgS5Gx9aP35X";
+$dbWriter="FvMqk1Aarwiw";
+$dbAdmin="oDiWj0UR3kr8";
+?>
---
+++ sitemap.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<a href="https://algoldma.w3.uvm.edu/cs148/assignment1.0/index.php">Sample Tables (Assignment 1.0)</a>
